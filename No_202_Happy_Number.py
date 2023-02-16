a79e9b47938ee0fa3d14447b07c57f802eb941cc
Jinhwan Jeon <121348144+jeonjnh@users.noreply.github.com>
2023-02-16 09:01:39 +0000

    add test case

--- No_202_Happy_Number.py
+++ No_202_Happy_Number.py
@@ -1,26 +1,30 @@
 class Solution:
-    def isHappy(self, n: int) -> bool:
-        if(n==1):
-            return True
-            
-        def calnumber(num):
-            sum = 0
-            while(num>0):
-                sum += (num%10)**2
-                num = num//10
-            return sum
 
-        num_list = []
-        while(n != 1):
-            n = calnumber(n)
-            if n in num_list:
-                return False
-            elif n == 1:
-                return True
-            else:
-                num_list.append(n)
+  def isHappy(self, n: int) -> bool:
+    if (n == 1):
+      return True
 
+    def calnumber(num):
+      sum = 0
+      while (num > 0):
+        sum += (num % 10)**2
+        num = num // 10
+      return sum
+
+    num_list = []
+    while (n != 1):
+      n = calnumber(n)
+      if n in num_list:
         return False
+      elif n == 1:
+        return True
+      else:
+        num_list.append(n)
+
+    return False
+
 
 answer = Solution().isHappy(19)
+print(answer)
+answer = Solution().isHappy(2)
 print(answer)
