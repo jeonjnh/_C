9cab933eca1a3770128273add31f65b66639c8fa
JEONJNH <121348144+jeonjnh@users.noreply.github.com>
2024-03-11 09:47:32 +0000

    add 791. Custom Sort String

---
+++ No_791_Custom_Sort_String.py
@@ -0,0 +1,63 @@
+"""
+791. Custom Sort String
+
+You are given two strings order and s. All the characters of order are unique and were sorted in some custom order previously.
+
+Permute the characters of s so that they match the order that order was sorted. More specifically, if a character x occurs before a character y in order, then x should occur before y in the permuted string.
+
+Return any permutation of s that satisfies this property.
+
+
+
+Example 1:
+
+Input:  order = "cba", s = "abcd" 
+
+Output:  "cbad" 
+
+Explanation: "a", "b", "c" appear in order, so the order of "a", "b", "c" should be "c", "b", and "a".
+
+Since "d" does not appear in order, it can be at any position in the returned string. "dcba", "cdba", "cbda" are also valid outputs.
+
+Example 2:
+
+Input:  order = "bcafg", s = "abcd" 
+
+Output:  "bcad" 
+
+Explanation: The characters "b", "c", and "a" from order dictate the order for the characters in s. The character "d" in s does not appear in order, so its position is flexible.
+
+Following the order of appearance in order, "b", "c", and "a" from s should be arranged as "b", "c", "a". "d" can be placed at any position since it's not in order. The output "bcad" correctly follows this rule. Other arrangements like "bacd" or "bcda" would also be valid, as long as "b", "c", "a" maintain their order.
+"""
+
+
+class Solution:
+
+  def customSortString(self, order: str, s: str) -> str:
+    # generate dictinary for order
+    char_count = {char: 0 for char in order}
+
+    # count how many string in s
+    for c in s:
+      if c in char_count:
+        char_count[c] += 1
+
+    sorted_s = ''
+
+    # add sting in order
+    for c in order:
+      sorted_s += c * char_count[c]
+
+    # add string not in order
+    for c in s:
+      if c not in order:
+        sorted_s += c
+
+    return sorted_s
+
+order = "cba"
+s = "abcd" 
+Output = "cbad"
+
+ans = Solution().customSortString(order, s)
+print("output= ", Output, "result= ", ans)
